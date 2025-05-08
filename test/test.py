e813e5de8173ac77a5d88c6135c107a259070075
0kq-github <0kq@0kqnet.work>
2025-05-09 08:19:18 +0900

    update assets

--- test/test.py
+++ test/test.py
@@ -4,13 +4,12 @@ sys.path.append('../')
 from enkanetwork import EnkaNetworkAPI, Language
 import asyncio
 import logging
+from pprint import pprint
 
 async def main():
     client = EnkaNetworkAPI(lang=Language.JP,debug=True)
-    resp = await client.fetch_user_by_uid(800857830)
-    print(resp.player.tower_act)
+    resp = await client.fetch_user_by_uid(854440964)
+    pprint({c.name:c.skills for c in resp.characters})
     print("fetch user OK")
-    await client.fetch_user_by_username("tankload")
-    print("fetch hoyos OK")
 
 asyncio.run(main())
